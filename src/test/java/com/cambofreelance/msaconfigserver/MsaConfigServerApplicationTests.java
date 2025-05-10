e3d2a87255d7d396acbbd7f3ff87cde5347e2e03
dinsarendev <dinsarendev@gmail.com>
2025-05-10 10:56:09 +0700

    First Init project
    First Init project

---
+++ src/test/java/com/cambofreelance/msaconfigserver/MsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cambofreelance.msaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
